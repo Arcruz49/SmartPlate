b14cdf2704fde53f72f779a471298434a03d2cd9
Arthur Cruz <arthurcruzdbp@gmail.com>
2026-01-30 18:00:32 -0300

    funciona agora o mapeamento de enums do banco com os enums do backend

--- Program.cs
+++ Program.cs
@@ -6,11 +6,26 @@ using SmartPlate.Application.Interfaces;
 using SmartPlate.Application.UseCases;
 using SmartPlate.Application.Security;
 using SmartPlate.Infrastructure.Data;
+using System.Text.Json.Serialization;
+using Npgsql;
+using SmartPlate.Domain.Enums;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+
+dataSourceBuilder.MapEnum<BiologicalSex>("biological_sex");
+dataSourceBuilder.MapEnum<TrainingType>("training_type");
+dataSourceBuilder.MapEnum<TrainingIntensity>("training_intensity");
+dataSourceBuilder.MapEnum<DailyActivityLevel>("daily_activity_level");
+dataSourceBuilder.MapEnum<UserGoals>("user_goal");
+
+var dataSource = dataSourceBuilder.Build();
+
 builder.Services.AddDbContext<Context>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(dataSource));
 
 var key = builder.Configuration["Jwt:Key"];
 builder.Services.AddAuthentication(options =>
@@ -37,7 +52,12 @@ builder.Services.AddScoped<ILoginUseCase, LoginUseCase>();
 builder.Services.AddScoped<IUserDataCreateCase, UserDataCreateCase>();
 builder.Services.AddScoped<JwtTokenGenerator>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+    
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -50,10 +70,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
