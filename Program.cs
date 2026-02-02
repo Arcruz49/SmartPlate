36eee25faffdcba5795b791abe8c2f7dd7ced494
Arthur Cruz <arthurcruzdbp@gmail.com>
2026-02-02 20:51:31 -0300

    fix cors

--- Program.cs
+++ Program.cs
@@ -23,7 +23,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:5173",
                 "http://192.168.1.69:5173",
                 "http://100.94.132.33:5173",
-                "http://100.124.94.117:5173"
+                "http://100.124.94.117:5173",
+                "http://192.168.12.122:5173"
             )
             .AllowAnyMethod()
             .AllowAnyHeader()
